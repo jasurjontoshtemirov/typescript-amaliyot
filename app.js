a3854d3b380cd78940df06d43efd81ac5042f15e
Jasurjon023 <k6yd023@gmail.com>
2025-07-22 19:37:44 +0500

    typescript

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 "use strict";
-const fullName="John Doe";
-console.log(fullName);
+// function getName(firstName:string,lastName:string):string{
+//     return `${firstName} ${lastName}`;
+// }
+// const data = getName("John", "Doe");
+// console.log(data);
+// const getData=(age:number):number=>{
+//     return age;
+// }
+//  const rea=getData(16);
+// console.log(rea);
+// const person={
+//     firstName:"Jasurjon",
+//     lastName:"Toshtemirov",
+//     skills:{
+//         programming:"Javascript",
+//         design:"Figma",
+//     }
+// }
+// function getName(data: {firstName:string,lastName:string}){
+//     return `${data.firstName} ${data.lastName}`
+// }
+// const person1=getName(person);
+// console.log(person1);
+// const cars:string[]=["BMW","Mercedes","Audi","Tesla"];
+// const numbers:number[]=[1,2,3,4,5,6,7,8,9,10];
+// const bools:boolean[]=[true,false,true,false];
+// const uppercase=cars
+//     .map(car=>car.toUpperCase())
+//     .filter(car=>car.startsWith("B"))
+// console.log(uppercase);
+// const mixed:(string | number | boolean)[]=["Jasurjon", 16, true, "Toshtemirov", 18, false];
+// console.log(mixed);
+// Typle
+const person = ["JasurjonToshtemirov", 17];
+person.push("Jizzax");
+console.log(person);
+console.log(person[0]);
+console.log(person[1]);
+// console.log(person[2]);
